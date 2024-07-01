07ca976bc96bfb75c5da71ff27e3e7b157c45a34
Lakkana.Perera <Lakkana.Perera@jato.com>
2024-07-01 11:04:21 +0100

    Add project files.

---
+++ ServiceBusPeek/Function1.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using PushVehicleIndexToServiceBus;
+
+namespace ServiceBusPeek
+{
+    public static class PeekMessage
+    {
+        [FunctionName("Peek")]
+        public static async Task<IActionResult> Peek(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string responseMessage = ServieBusQueue.PeekMessageAsync();
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
---
+++ ServiceBusPeek/ServieBusQueue.cs
@@ -0,0 +1,60 @@
+﻿using Azure.Messaging.ServiceBus;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace PushVehicleIndexToServiceBus
+{
+    public class ServieBusQueue
+    {
+        public async static Task SendMessageAsync(string serviceBusMessage)
+        {
+            try
+            {
+                string queueName = "vehicleindexincrements";
+                ServiceBusClient serviceBusClient = new ServiceBusClient("Endpoint=sb://jato-archsb.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=lNSZ3xbcaeIXPS+EKlcfcU6Rrw/llVjhDMplhR/Owkg=", new ServiceBusClientOptions()
+                {
+                    TransportType = ServiceBusTransportType.AmqpWebSockets
+                });
+                ServiceBusSender serviceBusSender = serviceBusClient.CreateSender(queueName);
+                await serviceBusSender.SendMessageAsync(new ServiceBusMessage(serviceBusMessage));
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public static string PeekMessageAsync()
+        {
+            try
+            {
+                string queueName = "vehicleindexincrements";
+                ServiceBusClient serviceBusClient = new ServiceBusClient("Endpoint=sb://jato-archsb.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=lNSZ3xbcaeIXPS+EKlcfcU6Rrw/llVjhDMplhR/Owkg=", new ServiceBusClientOptions()
+                {
+                    TransportType = ServiceBusTransportType.AmqpWebSockets
+                });
+                var serviceBusReceiver = serviceBusClient.CreateReceiver(queueName);
+                var message = serviceBusReceiver.PeekMessagesAsync(1).Result;
+                if (message.Any())
+                {
+                    Console.WriteLine(message.First().Body);
+                    return message.First().Body.ToString();
+                }
+                else
+                {
+                    Console.WriteLine(@"Queue is Empty");
+                    return "The Queue is Empty";
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }   
+    }
+}
